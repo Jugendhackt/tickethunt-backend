1f36ca4b39604cd70ea089e2f1b49675aa293a1f
pajowu <github@ca.pajowu.de>
2016-06-11 14:57:58 +0300

    fix typo, remove area field

--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ from django.contrib.gis.db import models
 
 # Create your models here.
 class TicketTypeAlias(models.Model):
-    name = models.CharField(max=length=256)
+    name = models.CharField(max_length=256)
     def __str__(self):
         return self.name
 
@@ -10,7 +10,6 @@ class TicketType(models.Model):
     name = models.CharField(max_length=256)
     comment = models.CharField(max_length=500)
     alias = models.ManyToManyField(TicketTypeAlias)
-    area = models.PolygonField()
 
 class Ticket(models.Model):
     location = models.PointField()
