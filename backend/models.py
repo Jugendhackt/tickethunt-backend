d64e3c08071c610a181eab0e873723f937800932
jasle <jas.le@web.de>
2016-06-11 13:43:09 +0200

    Updated models

--- backend/models.py
+++ backend/models.py
@@ -1,14 +1,21 @@
 from django.contrib.gis.db import models
 
 # Create your models here.
-class TicketType(models.Model):
-    name = models.CharField(max_length=256)
+class TicketTypeAlias(models.Model):
+    name = models.CharField(max=length=256)
     def __str__(self):
         return self.name
+
+class TicketType(models.Model):
+    name = models.CharField(max_length=256)
+    comment = models.CharField(max_length=500)
+    alias = models.ManyToManyField(TicketTypeAlias)
+    area = models.PolygonField()
 
 class Ticket(models.Model):
     location = models.PointField()
     ticket_type = models.ManyToManyField(TicketType)
-    description = models.CharField(max_length=500)
+    persons = models.IntegerField()
+    comment = models.CharField(max_length=500)
     image = models.ImageField(upload_to='./tickets/', null=True)
     valid_until = models.DateTimeField()
